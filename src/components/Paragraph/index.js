5a03f58b72ba40adf1868fb51caad99175894a7e
TTB <tranthaibinh0@gmail.com>
2023-01-18 00:33:07 +0700

    init source

---
+++ src/components/Paragraph/index.js
@@ -0,0 +1,8 @@
+import styles from "./Paragraph.module.css";
+
+console.log(styles);
+function Paragraph() {
+  return <p className={styles.paragraph}>ssdhfjsdhf</p>;
+}
+
+export default Paragraph;
